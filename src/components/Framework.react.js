236fda3212c0effaa21284650141be75667bc67f
Luigi Poole <luigipoole@outlook.com>
2016-02-11 14:46:50 -0800

    notify that we cant sync without net

--- src/components/Framework.react.js
+++ src/components/Framework.react.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import notifier from 'node-notifier'
+import isOnline from 'is-online'
+import path from 'path'
 import Sidebar from './Sidebar.react'
 import Settings from './Settings.react'
 
 
 export default class Framework extends React.Component {
-
 	componentWillMount() {
 		injectTapEventPlugin()
 	}
+	componentDidMount() {
+		isOnline((err, online) => {
+			if (!err && online) return
 
+			notifier.notify({
+				title: 'No Internet Connection Detected',
+				message: 'Wallpaper will be unable to sync while disconnected from the Internet',
+				icon: path.join(__dirname, '../../', 'images', 'Bing-logo-blue.png'),
+				sound: false,
+			})
+		})
+	}
 	render() {
 		return (
 			<div>
