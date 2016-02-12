c3b478616bbbe95ac6faffdc6dc1fdea0e04214a
Luigi Poole <luigipoole@outlook.com>
2016-02-11 23:06:48 -0800

    fix jayess's mess

--- src/browser.js
+++ src/browser.js
@@ -1,57 +1,56 @@
-import app from 'app';
-import BrowserWindow from 'browser-window';
-import shell from 'shell';
-import path from 'path';
-import yargs from 'yargs';
+import app from 'app'
+import BrowserWindow from 'browser-window'
+import shell from 'shell'
+import path from 'path'
+import yargs from 'yargs'
 
 
-const args = yargs(process.argv.slice(1)).wrap(100).argv;
-var minimzeInfoShown = false;
+const args = yargs(process.argv.slice(1)).wrap(100).argv
+var minimzeInfoShown = false
 
 
 app.on('ready', () => {
 
-    const minWidth = 720;
-    const minHeight = 445;
+    const minWidth = 900
+    const minHeight = 445
 
     const mainWindow = new BrowserWindow({
         minWidth,
         minHeight,
-        width: 760,
+        width: minWidth,
         height: 460,
         resizable: true,
         icon: 'images/Bing-logo-blue.png',
-        backgroundColor: '#fff',
         title: 'Photon Wallpaper',
         center: true,
         'auto-hide-menu-bar': true,
         frame: true,
         show: false
-    });
+    })
 
     if (args.dev) {
-        mainWindow.show();
-        mainWindow.toggleDevTools();
-        mainWindow.focus();
+        mainWindow.show()
+        mainWindow.toggleDevTools()
+        mainWindow.focus()
         console.info('Dev Mode Active: Developer Tools Enabled.')
     }
 
-    mainWindow.loadURL(path.normalize('file://' + path.join(__dirname, '../index.html')));
+    mainWindow.loadURL(path.normalize('file://' + path.join(__dirname, '../index.html')))
 
-    mainWindow.webContents.on('new-window', event => event.preventDefault());
+    mainWindow.webContents.on('new-window', event => event.preventDefault())
 
     mainWindow.webContents.on('will-navigate', (event, url) => {
         if (!url.includes('build/index.html'))
-            event.preventDefault();
+            event.preventDefault()
     });
 
     mainWindow.webContents.on('did-finish-load', () => {
-        mainWindow.show();
-        mainWindow.focus();
-    });
+        mainWindow.show()
+        mainWindow.focus()
+    })
 
-    mainWindow.on('close', app.quit);
+    mainWindow.on('close', app.quit)
 });
 
 
-app.on('window-all-closed', app.quit);
+app.on('window-all-closed', app.quit)
