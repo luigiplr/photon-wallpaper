dc8bddef8240d840c0a390d7a5091bff5ea9f6e6
Luigi Poole <luigipoole@outlook.com>
2016-02-11 21:39:22 -0800

    get bing wallpaper working

--- src/utils/wallpaperUtil.js
+++ src/utils/wallpaperUtil.js
@@ -4,7 +4,7 @@ import request from 'request'
 import fs from 'fs'
 import path from 'path'
 import {
-    app
+	app
 }
 from 'remote';
 
@@ -13,22 +13,37 @@ import AppStore from '../stores/appStore'
 const appUpdateDir = path.join(app.getPath('userData'), 'wallpaper_cache');
 
 if (!fs.existsSync(appUpdateDir))
-    fs.mkdirSync(appUpdateDir);
+	fs.mkdirSync(appUpdateDir);
+
+
 
 const syncUp = () => {
 	const {
 		provider, resolution, resolutionOptions
 	} = AppStore.getState();
 
-
 	switch (provider) {
 		case 'bing':
 
-			const url `http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=${AppStore.getState().region}`
+			request.get({
+				url: `http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=${AppStore.getState().region}`,
+				json: true
+			}, (error, response, body) => {
+				if (error || response.statusCode !== 200 || !body || !body.images || !body.images.length > 0) return
 
-			bingWallpaper({
-				locale: AppStore.getState().region
-			})
+				const image = `http://www.bing.com/${body.images[0].urlbase}_${resolution.replace('_', 'x')}.jpg`
+
+				const localPath = path.join(appUpdateDir, path.basename(image))
+
+				if (!fs.existsSync(localPath))
+					request
+					.get(image)
+					.pipe(fs.createWriteStream(localPath))
+					.on('finish', () => wallpaper.set(localPath))
+				else
+					wallpaper.set(localPath)
+			});
+
 			break
 	}
 }
