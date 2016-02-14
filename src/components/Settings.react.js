079539d0b21aeb30217d804d0fa438d4141ae55a
Luigi Poole <luigipoole@outlook.com>
2016-02-14 13:46:18 -0800

    Fix indent hell

--- src/components/Settings.react.js
+++ src/components/Settings.react.js
@@ -127,22 +127,21 @@ export default class Settings extends React.Component {
         			}
         		</SelectField>
 
-        		<If test={providers[this.state.provider].monitorOptions && providers[this.state.provider].monitorOptions.length > 1}>
-        			<SelectField
-        				value={this.state.monitors}
-        				onChange={(event, index, monitors) => AppActions.monitorChange(monitors)}
-        				{...feildStyles}
-                     	fullWidth={true}
-                      	floatingLabelText='Monitors'
-                      	>
-                      	{
-                    		providers[this.state.provider].monitorOptions.map(option => {
-                        			return <MenuItem key={option} value={option} primaryText={option}/>
-                    		})
-                    		}
-                    </SelectField>
-                </If>
-
+    			<If test={providers[this.state.provider].monitorOptions && providers[this.state.provider].monitorOptions.length > 1}>
+            		<SelectField
+          				value={this.state.monitors}
+          				onChange={(event, index, monitors) => AppActions.monitorChange(monitors)}
+          				{...feildStyles}
+          				fullWidth={true}
+          				floatingLabelText='Monitors'
+        				>
+        				{
+        				providers[this.state.provider].monitorOptions.map((option, idx) => {
+        					return <MenuItem key={idx + 1} value={option} primaryText={option}/>
+        				})
+        				}
+        			</SelectField>
+        		</If>
 
         		{this.getSettings.bind(this, this.state.provider)()}
         		
