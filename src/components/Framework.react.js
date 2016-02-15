9c7c47c1c8c7ddb60084a5de2ce1eb718e4cfc39
Luigi Poole <luigipoole@outlook.com>
2016-02-14 16:59:27 -0800

    add analytics & check for online connection on app start

--- src/components/Framework.react.js
+++ src/components/Framework.react.js
@@ -6,7 +6,9 @@ from 'material-ui'
 import ThemeManager from 'material-ui/lib/styles/theme-manager'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import notifier from 'node-notifier'
+import analytics from 'universal-analytics'
 import path from 'path'
+import isOnline from 'is-online'
 
 import Sidebar from './Sidebar.react'
 import Settings from './Settings.react'
@@ -34,18 +36,25 @@ export default class Framework extends React.Component {
 
 	componentDidMount() {
 		AppStore.listen(this.onChange)
-			/*
-				isOnline((err, online) => {
-					if (!err && online) return
-					notifier.notify({
-						title: 'No Internet Connection Detected',
-						message: 'Wallpaper will be unable to sync while disconnected from the Internet',
-						icon: path.join(__dirname, '../../', 'images', 'Bing-logo-blue.png'),
-						sound: false,
-					})
-				})
 
-			*/
+		const ga = analytics('UA-67206995-7', this.state.analiticsID, {
+			https: true
+		})
+
+		if (process.env.NODE_ENV === 'development')
+			ga.pageview('app/development').send()
+		else
+			ga.pageview('app/start').send()
+
+		isOnline((err, online) => {
+			if (!err && online) return
+			notifier.notify({
+				title: 'No Internet Connection Detected',
+				message: 'Wallpaper will be unable to sync while disconnected from the Internet',
+				icon: path.join(__dirname, '../../', 'images', 'Bing-logo-blue.png'),
+				sound: false,
+			})
+		})
 	}
 
 	componentWillUnmount() {
